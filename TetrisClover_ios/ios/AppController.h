4a71efa7fbe6c2fa482fac77e66c13efe283519e
zhang_ying <zhang_ying@zhang-ying-no-MacBook-Pro.local>
2014-03-06 23:39:10 +0900

    Initial Commit

---
+++ TetrisClover_ios/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TetrisClover_iosAppController.h
+//  TetrisClover_ios
+//
+//  Created by zhang_ying on 14-3-6.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TetrisClover_ios/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TetrisClover_iosAppController.h
+//  TetrisClover_ios
+//
+//  Created by zhang_ying on 14-3-6.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
